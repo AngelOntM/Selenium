35354e459ca926f1aca8281c9b72cae537f150c0
Angel Jahaziel Ontiveros Mendez <94997082+AngelOntM@users.noreply.github.com>
2024-07-17 02:24:04 -0600

    Posible practica final

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from selenium.webdriver.common.by import By
 import time
 from openpyxl import Workbook
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import StaleElementReferenceException
+import os
+from dotenv import load_dotenv
 
 class WebScraper(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def execute_actions(self, actions):
+    def execute_actions(self, actions, ws, headers_written):
         driver = self.driver
-        elements = None
-        columns = []
         for action in actions:
             if action['action'] == 'click':
                 element = driver.find_element(getattr(By, action['by']), action['value'])
@@ -22,7 +25,11 @@ class WebScraper(unittest.TestCase):
             elif action['action'] == 'input':
                 element = driver.find_element(getattr(By, action['by']), action['value'])
                 element.clear()
-                element.send_keys(action['input_value'])
+                if action.get('hidden', False):
+                    input_value = os.getenv(action['input_value'])
+                else:
+                    input_value = action['input_value']
+                element.send_keys(input_value)
             elif action['action'] == 'select':
                 select_element = driver.find_element(getattr(By, action['by']), action['value'])
                 select = Select(select_element)
@@ -32,56 +39,68 @@ class WebScraper(unittest.TestCase):
                 radio_element.click()
             elif action['action'] == 'checkbox':
                 checkbox_element = driver.find_element(getattr(By, action['by']), action['value'])
-                if action.get('check', True):
-                    if not checkbox_element.is_selected():
-                        checkbox_element.click()
+                if checkbox_element.is_selected() != action.get('selected', True):
+                    checkbox_element.click()
+            elif action['action'] == 'keyboard':
+                if 'element' in action:
+                    element = driver.find_element(getattr(By, action['by']), action['value'])
+                    element.send_keys(getattr(Keys, action['key'].upper()))
                 else:
-                    if checkbox_element.is_selected():
-                        checkbox_element.click()
+                    action_chain = ActionChains(driver)
+                    action_chain.send_keys(getattr(Keys, action['key'].upper())).perform()
             elif action['action'] == 'find_elements':
                 elements = driver.find_elements(getattr(By, action['by']), action['value'])
                 columns = action['columns']
+                headers = [column['header'] for column in columns]
+
+                if not headers_written:
+                    ws.append(headers)
+                    headers_written = True
+
+                for x, element in enumerate(elements, start=1):
+                    row = []
+                    for column in columns:
+                        try:
+                            selector = column['selector_pattern'].format(x=x)
+                            cell_data = element.find_element(By.CSS_SELECTOR, selector).text
+                            row.append(cell_data)
+                        except StaleElementReferenceException:
+                            print(f"Error al procesar elemento: el elemento se ha vuelto obsoleto.")
+                            row.append("")  # Añadir una celda vacía en caso de error
+                        except Exception as e:
+                            print(f"Error al procesar elemento: {e}")
+                            row.append("")  # Añadir una celda vacía en caso de error
+                    ws.append(row)
+
             if 'wait' in action:
                 time.sleep(action['wait'])
-        return elements, columns
+        return headers_written
 
     def test_web_scraper(self):
-        with open('config1.json') as f:
+        load_dotenv(dotenv_path='config.env')
+        with open('config1.json', encoding='utf-8') as f:
             config = json.load(f)
 
         driver = self.driver
         driver.get(config['start_url'])
-        time.sleep(2)
-
-        elements, columns = self.execute_actions(config['actions'])
+        time.sleep(5)
 
         # Crea un nuevo libro de Excel
         wb = Workbook()
         ws = wb.active
         ws.title = "Data Extracted"
+        headers_written = False
 
-        # Escribe la cabecera del archivo Excel
-        headers = [column['header'] for column in columns]
-        ws.append(headers)
-
-        for x, element in enumerate(elements, start=1):
-            row = []
-            for column in columns:
-                try:
-                    selector = column['selector_pattern'].format(x=x)
-                    cell_data = element.find_element(By.CSS_SELECTOR, selector).text
-                    row.append(cell_data)
-                except Exception as e:
-                    print(f"Error al procesar elemento: {e}")
-                    print(element.get_attribute('outerHTML'))
-                    row.append("")  # Añadir una celda vacía en caso de error
-            ws.append(row)
+        try:
+            headers_written = self.execute_actions(config['actions'], ws, headers_written)
+        except Exception as e:
+            print(f"Error general al ejecutar acciones: {e}")
 
         # Guarda el archivo Excel
         wb.save(config['output']['filename'])
 
     def tearDown(self):
-        self.driver.close()
+        self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
